623421af12e3ca1c244badf0f7f58620e0724a8a
rgarci <30447377+rgarci@users.noreply.github.com>
2017-09-10 14:06:23 -0500

    Delete UnificarRenglon.py

--- álgebra lineal/UnificarRenglon.py
+++
@@ -1,9 +0,0 @@
-"""Esta función vuelve 1 el pibote de un renglón"""
-
-
-def UnificarRenglon(tam, matriz, r):
-    for j in range(tam):
-        if (matriz[r][j]!=0):
-            aux = matriz[r][j]
-            for i in range(tam):
-                matriz[r][i] = (matriz[r][i]/aux)
